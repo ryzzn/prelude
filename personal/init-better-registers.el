c92ba64a2f18b869ff8c9acae078a0c6533b34ee
Ryan <a@sydi.org>
2014-09-20 01:12:04 +0800

    judge file existence before load file `better-register-save-file'

--- personal/init-better-registers.el
+++ personal/init-better-registers.el
@@ -1,6 +1,10 @@
 (require 'better-registers)
+
 (better-registers-install-save-registers-hook)
-(load better-registers-save-file)
+
+(if (file-exists-p better-registers-save-file)
+    (load better-registers-save-file)
+  )
 
 ;;; I don't like bind C-j to it especially in minibuffer.
 (define-key better-registers-map "\C-j" nil)
