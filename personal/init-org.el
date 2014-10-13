cf64b0aedabaf2dc07e10feee3705fde8e07857f
Ryan <a@sydi.org>
2014-10-13 15:08:09 +0800

    add back agenda things

--- personal/init-org.el
+++ personal/init-org.el
@@ -83,36 +83,36 @@
      ))
 
 
-;; (eval-after-load 'org
-;;   '(progn
-;;      (custom-set-variables
-;;       '(org-agenda-files (quote ("~/personal/todo.org")))
-;;       '(org-default-notes-file "~/personal/notes.org")
-;;       '(org-agenda-span 7)
-;;       '(org-deadline-warning-days 14)
-;;       '(org-agenda-show-all-dates t)
-;;       '(org-agenda-skip-deadline-if-done t)
-;;       '(org-agenda-skip-scheduled-if-done t)
-;;       '(org-agenda-start-on-weekday nil)
-;;       '(org-reverse-note-order t)
-;;       '(org-fast-tag-selection-single-key (quote expert))
-;;       '(org-agenda-custom-commands
-;;         (quote (("d" todo "DELEGATED" nil)
-;;                 ("c" todo "DONE|DEFERRED|CANCELLED" nil)
-;;                 ("w" todo "WAITING" nil)
-;;                 ("W" agenda "" ((org-agenda-span 21)))
-;;                 ("A" agenda ""
-;;                  ((org-agenda-skip-function
-;;                    (lambda nil
-;;                      (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
-;;                   (org-agenda-span 1)
-;;                   (org-agenda-overriding-header "Today's Priority #A tasks: ")))
-;;                 ("u" alltodo ""
-;;                  ((org-agenda-skip-function
-;;                    (lambda nil
-;;                      (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
-;;                                                (quote regexp) "\n]+>")))
-;;                   (org-agenda-overriding-header "Unscheduled TODO entries: ")))))))
-;;      (message "load org-mode")))
+(eval-after-load 'org
+  '(progn
+     (custom-set-variables
+      '(org-agenda-files (quote ("~/personal/todo.org" "~/personal/org/notes.org")))
+      '(org-default-notes-file "~/personal/notes.org")
+      '(org-agenda-span 7)
+      '(org-deadline-warning-days 14)
+      '(org-agenda-show-all-dates t)
+      '(org-agenda-skip-deadline-if-done t)
+      '(org-agenda-skip-scheduled-if-done t)
+      '(org-agenda-start-on-weekday nil)
+      '(org-reverse-note-order t)
+      '(org-fast-tag-selection-single-key (quote expert))
+      '(org-agenda-custom-commands
+        (quote (("d" todo "DELEGATED" nil)
+                ("c" todo "DONE|DEFERRED|CANCELLED" nil)
+                ("w" todo "WAITING" nil)
+                ("W" agenda "" ((org-agenda-span 21)))
+                ("A" agenda ""
+                 ((org-agenda-skip-function
+                   (lambda nil
+                     (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
+                  (org-agenda-span 1)
+                  (org-agenda-overriding-header "Today's Priority #A tasks: ")))
+                ("u" alltodo ""
+                 ((org-agenda-skip-function
+                   (lambda nil
+                     (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
+                                               (quote regexp) "\n]+>")))
+                  (org-agenda-overriding-header "Unscheduled TODO entries: ")))))))
+     (message "load org-mode")))
 
 (provide 'init-org)
