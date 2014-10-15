3faa27156fd0be4fe263e489d49acc76ef417661
符风 <fufeng.syd@alipay.com>
2014-10-15 17:10:22 +0800

    add marmalade packege store

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -39,6 +39,7 @@
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("SC" . "http://joseito.republika.pl/sunrise-commander/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; set package-user-dir to be relative to Prelude install path
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
